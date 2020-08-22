c4674711f87c637af4d0c5b2238a8d646ef6ba67
Periapsises <45960263+Periapsises@users.noreply.github.com>
2020-08-22 18:27:13 -0400

    Replace entity damage event with PlayerDeathEvent

--- src/main/java/me/periapsis/playercapitator/listeners/PlayerHeadLoot.java
+++ src/main/java/me/periapsis/playercapitator/listeners/PlayerHeadLoot.java
@@ -2,34 +2,38 @@ package me.periapsis.playercapitator.listeners;
 
 import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 
 public class PlayerHeadLoot implements Listener {
     @EventHandler
-    private void onPlayerDeath( EntityDamageByEntityEvent event ) {
-        Entity ent = event.getEntity();
-        Entity killer = event.getDamager();
+    private void onPlayerDeath( PlayerDeathEvent deathEvent ) {
+        Player victim = (Player) deathEvent.getEntity();
 
-        if ( ent instanceof Player && killer instanceof Player ) {
-            Player ply = (Player) ent;
-
-            if ( ply.getHealth() - event.getDamage() <= 0 ) {
-                ItemStack skull = new ItemStack( Material.PLAYER_HEAD, 1 );
-
-                SkullMeta meta = (SkullMeta) skull.getItemMeta();
-                meta.setPlayerProfile( ply.getPlayerProfile() );
-                meta.setDisplayName( ply.getDisplayName() + "'s head" );
-                skull.setItemMeta( meta );
-
-                World world = ply.getWorld();
-                world.dropItem( ply.getLocation(), skull );
-            }
+        EntityDamageEvent damageEvent = deathEvent.getEntity().getLastDamageCause();
+        if ( !( damageEvent instanceof EntityDamageByEntityEvent ) ) {
+            return;
         }
+        EntityDamageByEntityEvent entityDamageEvent = (EntityDamageByEntityEvent) damageEvent;
+
+        if ( !( entityDamageEvent.getDamager() instanceof Player ) ) {
+            return;
+        }
+
+        ItemStack skull = new ItemStack( Material.PLAYER_HEAD, 1 );
+
+        SkullMeta meta = (SkullMeta) skull.getItemMeta();
+        meta.setPlayerProfile( victim.getPlayerProfile() );
+        meta.setDisplayName( victim.getDisplayName() + "'s head" );
+        skull.setItemMeta( meta );
+
+        World world = victim.getWorld();
+        world.dropItem( victim.getLocation(), skull );
     }
 }
